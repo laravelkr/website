a9954821808e71e3984937e0fd1e72dbe251958d
Jung-Soo Ahn <staringstar@gmail.com>
2020-11-30 17:07:13 +0900

    배너 링크 영역을 전체로 변경

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -151,13 +151,15 @@
             <div class="row fullscreen justify-content-center align-items-center">
                 <div class="col-md-10 col-12">
                     <div class="banner-content text-center mb-20">
+                        <a href="http://www.yes24.com/Product/Goods/95757831" target="_blank">
 {{--                        <h4 class="text-white mb-20 text-uppercase">Discover the Colorful World</h4>--}}
                         <h1 class="text-white mb-20 mt-80 text-uppercase">라라벨 Up & Running</h1>
                         <p class="text-white">처음부터 제대로 배우는 라라벨</p>
                         <img class="hello-illustration" src="//image.yes24.com/goods/95757831/800x0"
                              style="max-width:50%; height:auto;margin:auto;">
-                        <a href="http://www.yes24.com/Product/Goods/95757831" target="_blank"
-                           class="text-uppercase header-btn">구매하기</a>
+
+                        <span class="text-uppercase header-btn">구매하기</span>
+                        </a>
                     </div>
                 </div>
             </div>
