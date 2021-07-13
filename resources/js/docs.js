5ed957dc58d997d04933c48e39d4357c785cb6fb
kkame <kkamenet@gmail.com>
2021-07-13 12:42:52 +0900

    LNB 위치 유지 기능 추가.

--- resources/js/docs.js
+++ resources/js/docs.js
@@ -26,6 +26,25 @@ $(window).on('load', function () {
         wScroll > 300 ? $('#back-to-top').fadeIn() : $('#back-to-top').fadeOut();
     });
 
+
+    let $sidebar = $('.sidebar .sidebar-nav');
+    $sidebar.on('scroll', function () {
+        let wScroll = $(this).scrollTop();
+            if(!window.localStorage){
+                return false;
+            }
+            //로컬스토리지 저장하는 3가지 방법. 다 같은 방법.
+            localStorage.setItem("scroll-position", wScroll);
+    });
+
+    if(window.localStorage){
+        let wScroll = localStorage.getItem("scroll-position")
+        if(wScroll)
+            $sidebar.scrollTop(wScroll);
+    }
+
+
+
     let $body = $("body");
     $(".navbar-toggler").on('click', function () {
         $.cookie('navbar-toggle', $body.hasClass('sidebar-lg-show'), {path: '/'});
