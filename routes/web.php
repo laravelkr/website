1293c311d0c67ac184ca5880432cc5b2b90f9a5a
kkame <kkamenet@gmail.com>
2022-04-22 20:48:58 +0900

    미사용 라우트 제거.

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Auth::routes();
-
 Route::get('home', [HomeController::class, 'index']);
 
 Route::get('docs', function () {
