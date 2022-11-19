eb96a92568344eb619a911514dc40acf08859835
kkame <kkamenet@gmail.com>
2022-11-20 00:47:35 +0900

    meta keyword에 php 추가.

--- config/website.php
+++ config/website.php
@@ -17,7 +17,7 @@ return [
     'meta' => [
 
         'description' => 'laravel korea community 라라벨 코리아 커뮤니티 - 한글 매뉴얼',
-        'keywords' => '라라벨 laravel 한국어 매뉴얼 5.6 5.5 5.4 5.3 5.2 5.1',
+        'keywords' => 'php 라라벨 laravel 한국어 매뉴얼 5.6 5.5 5.4 5.3 5.2 5.1',
         'author' => 'laravel@laravel.kr',
         'robots' => 'index,follow',
 
